94431caccdd5e54915dd5569aabc1dc1d6d1c96c
Dabeer Shaikh <dab8106@gmail.com>
2020-08-09 16:28:45 +0530

    conflicts

--- compact.js
+++ compact.js
@@ -1,4 +1,5 @@
 alert( 'Hello, world! from Jerry changes learning to stash' );
+conflicts
 alert( 'Hello, world! from Jerry changes' );
 alert( 'Hello, world! from Jerry changes' );
 alert( 'Hello, world! from Jerry changes' );
